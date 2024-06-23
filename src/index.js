260f74cd9c9ea0b30b7ef73e1eff785c96bbb5d2
Winrick <winrick.web@gmail.com>
2024-06-23 16:10:06 +0430

    worked splide & jquery

--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
 import './assets/css/index.css'
 
 $(document).ready(function () {
-  $("div").click(function () {
+  $(".ttt").click(function () {
     alert('test');
   });
 });
+
+import Splide from '@splidejs/splide';
+
+const splideNojavanContent = new Splide('.splide-nojavan-content', {
+  direction: 'rtl',
+  pagination: false,
+  arrows: false,
+  loop: 'true',
+}).mount();
